9eaec8155c218f090f4b4cfd87ac3f6b317d3be6
tolmachev21 <tolmach0221@yandex.ru>
2023-03-11 22:55:31 +0500

    Первый запуск

--- script/index.js
+++ script/index.js
@@ -3,25 +3,20 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close-button
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 
 const openPopup = function() {
-    pop
+    popupElement.classList.add('popup_opened');
 }
 
-function togglePopupDisplay (evt) {
-    evt.preventDefault();
-    if (popupElement.classList.contains('popup_opened')) {
-        popupElement.classList.remove('popup_opened');
-    } else {
-        popupElement.classList.add('popup_opened');
-    }
-};
+const closePopup = function() {
+    popupElement.classList.remove('popup_opened');
+}
 
-popupCloseButtonElement.addEventListener('click', togglePopupDisplay);
-popupOpenButtonElement.addEventListener('click', togglePopupDisplay);
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
 
 const formElement = popupElement.querySelector('.popup__form');
 const formSubmitElement = formElement.querySelector('.popup__form_submit-button');
 const nameInputElement = formElement.querySelector('.popup__form_input_name');
-const jobInputElement = formElement.querySelector('.popup__form_input_job');// 
+const jobInputElement = formElement.querySelector('.popup__form_input_job');
 
 function handleFormSubmit (event) {
     event.preventDefault();
@@ -31,7 +26,16 @@ function handleFormSubmit (event) {
     const profileJobElemtnt = document.querySelector('.profile__job');
     profileNameElement.textContent = nameInputElement.value;
     profileJobElemtnt.textContent = jobInputElement.value;
+    closePopup();
 }; 
 
 formElement.addEventListener('submit', handleFormSubmit);
 
+const closePopupByClickOnOverlay = function (event) {
+    if (event.target !== event.currentTarget) {
+        return;
+    }
+    closePopup();
+}
+
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
