5eda6db197fc5d6c691f1f71c9ae320c5fa80cc4
Viktor Tolmachev <Tolmach0221@yandex.ru>
2023-03-13 14:37:27 +0500

    Вторая интерация

--- script/index.js
+++ script/index.js
@@ -1,26 +1,27 @@
+const profileNameElement = document.querySelector('.profile__name');
+const profileJobElemtnt = document.querySelector('.profile__job');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const formElement = popupElement.querySelector('.popup__form');
+const formSubmitElement = formElement.querySelector('.popup__submit-button');
+const nameInputElement = formElement.querySelector('.popup__input_field_name');
+const jobInputElement = formElement.querySelector('.popup__input_field_job');
 
 const openPopup = function() {
     popupElement.classList.add('popup_opened');
+    nameInputElement.value = profileNameElement.textContent;
+    jobInputElement.value = profileJobElemtnt.textContent;
 }
 
 const closePopup = function() {
     popupElement.classList.remove('popup_opened');
 }
 
-const formElement = popupElement.querySelector('.popup__form');
-const formSubmitElement = formElement.querySelector('.popup__submit-button');
-const nameInputElement = formElement.querySelector('.popup__input_field_name');
-const jobInputElement = formElement.querySelector('.popup__input_field_job');
-
 function handleFormSubmit (event) {
     event.preventDefault();
     nameInputElement.getAttribute('value');
     jobInputElement.getAttribute('value');
-    const profileNameElement = document.querySelector('.profile__name');
-    const profileJobElemtnt = document.querySelector('.profile__job');
     profileNameElement.textContent = nameInputElement.value;
     profileJobElemtnt.textContent = jobInputElement.value;
     closePopup();
